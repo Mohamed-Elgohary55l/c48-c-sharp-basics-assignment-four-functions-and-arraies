b74ff2021e3dd34f5c6ab9ff83275111857e8612
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-07-27 12:34:49 +0300

    Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,10 +8,16 @@
             //double[] prices = { 25.5, 40.0, 33.75 };
             //Console.WriteLine(prices[1]);
 
-    
+
             #endregion
 
-            #region Q2
+            #region Q2Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
+            //int[,] shelfcopis = 
+            //{
+            //{3,5 },
+            //{1,4 }
+            //       };
+            //Console.WriteLine(shelfcopis[1, 0]);
             #endregion
             #region Q3
             #endregion
