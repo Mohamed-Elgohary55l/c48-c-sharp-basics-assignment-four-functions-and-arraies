550f9616f1fdbc4bc984e8160b576bcb3d0347b4
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-07-27 12:29:04 +0300

    Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price (index 1).

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Q1 Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price (index 1).
+            //double[] prices = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(prices[1]);
+
+    
+            #endregion
+
+            #region Q2
+            #endregion
+            #region Q3
+            #endregion
+            #region Q4
+            #endregion
+            #region Q5
+            #endregion
+            #region Q6
+            #endregion
+            #region Q7
+            #endregion
+            #region Q8
+            #endregion
+            #region Q9
+            #endregion
+            #region Q10
+            #endregion
+            #region Q11
+            #endregion
+            #region Q12
+            #endregion
+            #region Q13
+            #endregion
+            #region Q14
+            #endregion
+
         }
     }
 }
